ccff5a307b4c873c20a4b6c839964a5eb283a3bc
rizl-ad <rizl@list.ru>
2025-08-30 00:13:58 +0300

    Create var.tf

---
+++ terr/work-1/task-2/p4/var.tf
@@ -0,0 +1,23 @@
+variable "yc_token" {
+    type = string
+    sensitive = true
+}
+
+variable "yc_cloud_id" {
+    type = string
+    sensitive = true
+}
+
+variable "yc_folder_id" {
+    type = string
+    sensitive = true
+}
+
+variable "yc_compute_default_zone" {
+    type = string
+}
+
+variable "vm_user_name" {
+    type = string
+    sensitive = true
+}
