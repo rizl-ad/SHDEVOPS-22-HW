36b6f8d482ff5d0300ecf73e8a2f676a8cf71c6a
rizl-ad <rizl@list.ru>
2025-08-30 00:12:36 +0300

    Update main.tf

--- terr/work-1/task-2/p4/main.tf
+++ terr/work-1/task-2/p4/main.tf
@@ -11,7 +11,6 @@ terraform {
   }
   required_version = ">=1.8.4"
 }
-provider "docker" {}
 
 provider "yandex" {
   token = var.yc_token
@@ -20,7 +19,27 @@ provider "yandex" {
   zone = var.yc_compute_default_zone
 }
 
+provider "docker" {
+  host = "ssh://${var.vm_user_name}@${data.yandex_compute_instance.vm_terr_w1_t2.network_interface.0.nat_ip_address}"
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+}
+
+data "yandex_compute_instance" "vm_terr_w1_t2" {
+  name = "vm-terr-w1-t2"
+}
+
+#resource "local_file" "ssh_config" {
+#  content = <<-EOF
+#    Host ${data.yandex_compute_instance.vm_terr_w1_t2.network_interface.0.nat_ip_address}
+#      HostName ${data.yandex_compute_instance.vm_terr_w1_t2.network_interface.0.nat_ip_address}
+#      User ${var.vm_user_name}
+#      IdentityFile ~/.ssh/ssh-key
+#  EOF
+#  filename = "~/.ssh/config"
+#}
+
 resource "random_password" "random_string" {
+  count = 2
   length      = 16
   special     = true
   min_upper   = 4
@@ -37,10 +56,10 @@ resource "docker_container" "mysql1" {
   image = docker_image.mysql.image_id
   name = "mysql1"
   env = [
-    "MYSQL_ROOT_PASSWORD=${random_password.random_string.result}",
+    "MYSQL_ROOT_PASSWORD=${random_password.random_string[0].result}",
     "MYSQL_DATABASE=wordpress",
     "MYSQL_USER=wordpress",
-    "MYSQL_PASSWORD=${random_password.random_string.result}",
+    "MYSQL_PASSWORD=${random_password.random_string[1].result}",
     "MYSQL_ROOT_HOST=%"
   ]
 
