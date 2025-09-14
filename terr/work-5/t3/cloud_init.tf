d903c8a9cbd810e79d9440bf0d2ba8cbbfc077a4
rizl-ad <rizl@list.ru>
2025-09-14 21:48:43 +0300

    Delete terr/work-5/t3 directory

--- terr/work-5/t3/cloud_init.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "cloud_init" {
-  content = templatefile(
-    "./cloud_init.tftpl", {
-        user_name = var.vm_user_name
-        pub_ssh_key = local.public_key
-    }
-  )
-  filename = "./cloud-init.yaml"
-}
